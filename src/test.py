a089d8046823ff8af5f55640c7d4e27115513850
TayamaYuto <tayama.yuto@icloud.com>
2023-07-28 15:47:35 +0900

    add : test.py

---
+++ src/test.py
@@ -0,0 +1,15 @@
+import numpy as np
+import cv2
+from insightface.app import FaceAnalysis
+
+image_file = "./data/input/test_image.jpg"
+img = cv2.imread(image_file)
+ 
+app = FaceAnalysis()
+app.prepare(ctx_id=0, det_size=(640, 640))
+
+faces = app.get(np.array(img))
+print("faces:" + str(len(faces)))
+
+rimg = app.draw_on(img, faces)
+cv2.imwrite("./data/output/test_output.jpg", rimg)
