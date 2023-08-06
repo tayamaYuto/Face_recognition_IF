24dc9b18776931250231bbd5b772a3aca7a02cf5
TayamaYuto <tayama.yuto@icloud.com>
2023-08-06 17:42:14 +0900

    add main.ipynb

--- src/test.py
+++ src/test.py
@@ -9,7 +9,6 @@ app = FaceAnalysis()
 app.prepare(ctx_id=0, det_size=(640, 640))
 
 faces = app.get(np.array(img))
-print("faces:" + str(len(faces)))
 
 rimg = app.draw_on(img, faces)
-cv2.imwrite("./data/output/test_output.jpg", rimg)
+cv2.imwrite('./data/output/test_output.jpg', rimg)
